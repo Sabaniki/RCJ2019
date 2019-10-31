0a82201ebb55a3fb0736dca49c43ecfa53cdb5b5
Sabaniki <sawada13@mccc.jp>
2019-10-31 18:35:24 +0900

    定義だけした

--- lib/Linetracer/Linetracer.h
+++ lib/Linetracer/Linetracer.h
@@ -2,12 +2,22 @@
 #define ___Class_Linetracer
 #pragma region includes
 #include "Arduino.h"
+#include "PhotoReflector.h"
+#include "Move.h"
 #pragma endregion
 class Linetracer {
 private:
-
+    Move manager;
+    PhotoReflector phts[5] = {
+        PhotoReflector(0),
+        PhotoReflector(1),
+        PhotoReflector(3),
+        PhotoReflector(4),
+        PhotoReflector(5),
+    };
 public:
     Linetracer();
+    void run();
 };
 
 #endif
--- lib/Move/Move.h
+++ lib/Move/Move.h
@@ -1,10 +1,17 @@
 #ifndef ___Class_Move
 #define ___Class_Move
+#include "Motor.h"
 class Move {
 private:
-
+    Motor motorL, motorR;
 public:
-    Move();
+    Move(Motor motorL, Motor motorR);
+    void straight(int speed);
+    void straight(int speed, int length);
+    void back(int speed);
+    void back(int speed, int length);
+    void right(int speed, bool rotate);
+    void left(int speed, bool rotate);
 };
 
 #endif
