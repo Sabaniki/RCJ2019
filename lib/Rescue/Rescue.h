9ce075c3bf8ce4639b5018fdc8c66c6fed5c8084
ShinoTakusan <ta4780@icloud.com>
2019-11-10 08:33:47 +0900

    VSCodeからコミットしてみた

--- lib/Rescue/Rescue.h
+++ lib/Rescue/Rescue.h
@@ -2,10 +2,30 @@
 #define ___Cpp_Resucue
 #include "Rescue.h"
 #include "Rescue.cpp"
+#include "Motor.h"
+#include "Motor.cpp"
+#include "RotaryEncoder.h"
+#include "RotaryEncoder.cpp"
+#include "Move.h"
+#include "Move.cpp"
+#include "ColorSensor.h"
+#include "ColorSensor.cpp"
+#include "PhotoReflector.h"
+#include "PhotoReflector.cpp"
+#include "Variables.h"
 
 class Rescue{
     private:
-
+        Move manager
+        PhotoReflector phts[5] = {
+        PhotoReflector(PHT_PIN_1),
+        PhotoReflector(PHT_PIN_2),
+        PhotoReflector(PHT_PIN_3),
+        PhotoReflector(PHT_PIN_4),
+        PhotoReflector(PHT_PIN_5),
+    };
+        Motor motorL, motorR
+        RotaryEncoder rotaryEncoder
     public:
 
 };
