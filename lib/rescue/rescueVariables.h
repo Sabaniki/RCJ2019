97531063abe6415b415e0227e193aa71bd19f240
Sabaniki <sawada13@mccc.jp>
2019-11-24 00:44:29 +0900

    rescue3

---
+++ lib/rescue/rescueVariables.h
@@ -0,0 +1,30 @@
+//----- サーボ -----//
+#define SRV_PIN_1 -1
+#define SRV_PIN_2 -1
+#define FRONT_UP_ANGLE 90
+#define FRONT_DOWN_ANGLE 0
+#define BACK_UP_ANGLE 90
+#define BACK_DOWN_ANGLE 0
+#define FRONT 1
+#define BACK 2
+#define GRADUALLY_FRONT 3
+#define GRADUALLY_BACK 4
+
+//----- 超音波 -----//
+#define ULT_ECHO_PIN_1 -1
+#define ULT_ECHO_PIN_2 -1
+#define ULT_TRIG_PIN_1 -1
+#define ULT_TRIG_PIN_2 -1
+//----- タッチセンサー -----//
+#define SW_PIN_1 -1
+#define SW_PIN_2 -1
+
+//----- ロータリーエンコーダ -----//
+#define ROTARY_ENCODER_READER_PIN_1 -1
+#define ROTARY_ENCODER_READER_PIN_2 -1
+#define LEFT90 -1
+#define RIGHT90 -1
+
+//----- その他 -----//
+#define BLOCK_LENGTH -1
+#define THRESHOLD_RRTE 100
